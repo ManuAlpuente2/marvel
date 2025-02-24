aeecd06f1e21701c3285ebcf7d9f7e462f438a67
Manu Alpuente <malpuente@smartvel.com>
2025-02-24 20:06:46 +0100

    Estructura basica

---
+++ src/server/index.js
@@ -0,0 +1,11 @@
+import md5 from "md5";
+import { privateKey, publicKey } from "./../constants";
+
+export const getMarvelCharacters = () => {
+  const timeStamp = new Date().getTime();
+  const hash = md5(`${timeStamp}${privateKey}${publicKey}`);
+  const url = `http://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}&limit=50`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.data);
+};
