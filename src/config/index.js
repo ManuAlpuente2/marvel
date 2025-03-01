6073d1b2f79be78b17bf69ba910a59612d7a1448
Manu Alpuente <malpuente@smartvel.com>
2025-03-01 19:29:53 +0100

    Refinamiento

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+const config = {
+  privateKey: "3f144a4296dfb54b9da73d1df3bf9fdce153e3ff",
+  publicKey: "5e8cf5c4668984bdff93cb7672cf029d",
+  apiUrl: "https://gateway.marvel.com/v1/public/",
+  cacheTime: 30000, // 86400000 =24 hours
+  charactersLimit: 50,
+  collectionsLimit: 20,
+  searchDebounceTime: 300,
+};
+
+export default config;
